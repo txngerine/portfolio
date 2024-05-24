53551513fae5a43f0c71244b431b1b29b742e983
Akshay TS <57423537+nyouibou@users.noreply.github.com>
2024-05-24 08:45:53 +0530

    updates

--- lib/presentation/about_me_screen/view/widgets/expcard.dart
+++ lib/presentation/about_me_screen/view/widgets/expcard.dart
@@ -8,62 +8,65 @@ class Expcard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Row(
-          children: [
-            Text(
-              "1",
-              style: NeededTextstyles.ultimatemaxwhite,
-            ),
-            SizedBox(width: 5),
-            Text(
-              "year of\nexperience",
-              style: NeededTextstyles.littletextdark,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              "10",
-              style: NeededTextstyles.ultimatemaxwhite,
-            ),
-            SizedBox(width: 5),
-            Text(
-              "projects\ncompleted",
-              style: NeededTextstyles.littletextdark,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              "8",
-              style: NeededTextstyles.ultimatemaxwhite,
-            ),
-            SizedBox(width: 5),
-            Text(
-              "technologies\nmastered",
-              style: NeededTextstyles.littletextdark,
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Text(
-              "100",
-              style: NeededTextstyles.ultimatemaxwhite,
-            ),
-            SizedBox(width: 5),
-            Text(
-              "number of\ncommits",
-              style: NeededTextstyles.littletextdark,
-            ),
-          ],
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Row(
+            children: [
+              Text(
+                "1",
+                style: NeededTextstyles.ultimatemaxwhite,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "year of\nexperience",
+                style: NeededTextstyles.littletextdark,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "10",
+                style: NeededTextstyles.ultimatemaxwhite,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "projects\ncompleted",
+                style: NeededTextstyles.littletextdark,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "8",
+                style: NeededTextstyles.ultimatemaxwhite,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "technologies\nmastered",
+                style: NeededTextstyles.littletextdark,
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Text(
+                "100",
+                style: NeededTextstyles.ultimatemaxwhite,
+              ),
+              SizedBox(width: 5),
+              Text(
+                "number of\ncommits",
+                style: NeededTextstyles.littletextdark,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
